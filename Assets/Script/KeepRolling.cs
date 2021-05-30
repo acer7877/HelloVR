aa942ecf693c359e3e540efe322250d2dad6fa84
acer7877 <46747235+acer7877@users.noreply.github.com>
2021-05-30 17:22:23 -0600

    windmill complete

---
+++ Assets/Script/KeepRolling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepRolling : MonoBehaviour
+{
+    public VRTK.Controllables.ArtificialBased.VRTK_ArtificialRotator Rotator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Rotator == null)
+            return;
+        //Debug.LogError(">>>> " + Rotator.GetValue().ToString());
+        float CurrentAngle = Rotator.GetValue();
+        if (CurrentAngle >= 180)
+            Rotator.SetValue(CurrentAngle - 360);
+        else if (CurrentAngle <= -180)
+            Rotator.SetValue(CurrentAngle + 360);
+        
+    }
+}
